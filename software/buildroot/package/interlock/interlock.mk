1a397d8cc8449cb21b9fd9539c862502e4ed2c93
Andrea Barisani <andrea@inversepath.com>
2016-04-13 17:57:53 +0200

    bump buildroot INTERLOCK revision

--- software/buildroot/package/interlock/interlock.mk
+++ software/buildroot/package/interlock/interlock.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-INTERLOCK_VERSION = 5422589e2ced3686b362dda148c83a063510fe5c
+INTERLOCK_VERSION = 8bd6c28dadd10a4dbdde15abe24d4fbe46463209
 INTERLOCK_SITE = $(call github,inversepath,interlock,$(INTERLOCK_VERSION))
 INTERLOCK_REPO = https://github.com/inversepath/interlock
 
