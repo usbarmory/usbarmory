183b54e3b29d5392073350cc105802f28583ba26
Andrea Barisani <andrea@inversepath.com>
2018-12-18 16:37:24 +0100

    bump caam-keyblob version

--- software/buildroot/package/caam-keyblob/caam-keyblob.mk
+++ software/buildroot/package/caam-keyblob/caam-keyblob.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-CAAM_KEYBLOB_VERSION = ecc82198c2f8c9f873e14d6dc9212fda6691f8bf
+CAAM_KEYBLOB_VERSION = 581e55f6d67e8d3a7734b9a83d6eac571db51a3c
 CAAM_KEYBLOB_SITE = $(call github,inversepath,caam-keyblob,$(CAAM_KEYBLOB_VERSION))
 
 CAAM_KEYBLOB_LICENSE = GPLv3
